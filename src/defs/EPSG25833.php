c412f9ee5f8d028430d3ad546363861719856a40
Jason Judge <jason.judge@consil.co.uk>
2015-09-20 10:59:46 +0100

    src scripts moved up a level.

---
+++ src/defs/EPSG25833.php
@@ -0,0 +1,4 @@
+<?php
+namespace proj4php;
+
+Proj4php::$defs["EPSG:25833"] = "+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs";
---
+++ src/defs/EPSG26912.php
@@ -0,0 +1,4 @@
+<?php
+namespace proj4php;
+
+Proj4php::$defs["EPSG26912"] = "+title=NAD83 / UTM zone 12N +proj=utm +zone=12 +a=6378137.0 +b=6356752.3141403";

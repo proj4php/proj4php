aa0ae8ef5004210915f7144fe89316f75af93444
Jason Judge <jason.judge@consil.co.uk>
2015-09-20 12:06:00 +0100

    Issue #5 Forgot to do "git add -A" when renaming files.

---
+++ src/LongLat.php
@@ -0,0 +1,26 @@
+<?php
+namespace proj4php;
+
+/**
+ * Author : Julien Moquet
+ * 
+ * Inspired by Proj4js from Mike Adair madairATdmsolutions.ca
+ *                      and Richard Greenwood rich@greenwoodmap.com 
+ * License: LGPL as per: http://www.gnu.org/copyleft/lesser.html 
+ */
+class LongLat
+{
+    public function init()
+    {
+    }
+
+    public function forward($pt)
+    {
+        return $pt;
+    }
+
+    public function inverse($pt)
+    {
+        return $pt;
+    }
+}

17fe353bc7d8bf24bee976f08733e1200157fa61
Christian WERNER <christian.werner@rizzistrasse.com>
2015-10-26 11:36:43 +0100

    Added Tests

--- test/Proj4phpTest.php
+++ test/Proj4phpTest.php
@@ -8,18 +8,18 @@ use proj4php\Point;
 class Proj4phpTest extends PHPUnit_Framework_TestCase
 {
 
-public function testTransform()
-{
+    public function testTransform()
+    {
 
-$proj4 = new Proj4php();
-$projL93 = new Proj('EPSG:2154',$proj4);
-$projWGS84 = new Proj('EPSG:4326',$proj4);
-$projLI = new Proj('EPSG:27571',$proj4);
-$projLSud = new Proj('EPSG:27563',$proj4);
-$projL72 = new Proj('EPSG:31370',$proj4);
-$proj25833 = new Proj('EPSG:25833',$proj4);
-$proj31468 = new Proj('EPSG:31468',$proj4);
-$proj5514 = new Proj('EPSG:5514',$proj4);
+        $proj4     = new Proj4php();
+        $projL93   = new Proj('EPSG:2154', $proj4);
+        $projWGS84 = new Proj('EPSG:4326', $proj4);
+        $projLI    = new Proj('EPSG:27571', $proj4);
+        $projLSud  = new Proj('EPSG:27563', $proj4);
+        $projL72   = new Proj('EPSG:31370', $proj4);
+        $proj25833 = new Proj('EPSG:25833', $proj4);
+        $proj31468 = new Proj('EPSG:31468', $proj4);
+        $proj5514  = new Proj('EPSG:5514', $proj4);
 
 // GPS
 // latitude        longitude
@@ -33,43 +33,80 @@ $proj5514 = new Proj('EPSG:5514',$proj4);
 // 601413.709   1125717.730
 //
 
-$pointSrc = new Point('652709.401','6859290.946',$projL93);
-$pointDest = $proj4->transform($projWGS84,$pointSrc);
+        $pointSrc  = new Point('652709.401', '6859290.946', $projL93);
+        $pointDest = $proj4->transform($projWGS84, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projLSud,$pointSrc);
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projLSud, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projWGS84,$pointSrc);
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projWGS84, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projLI,$pointSrc);
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projLI, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projL93,$pointSrc);
-
-
-
-$pointSrc = new Point('177329.253543','58176.702191');
-$pointDest = $proj4->transform($projL72,$projWGS84,$pointSrc);
-
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projWGS84,$projL72,$pointSrc);
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projL93, $pointSrc);
 
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projL72,$proj25833,$pointSrc);
+        $pointSrc  = new Point('177329.253543', '58176.702191');
+        $pointDest = $proj4->transform($projL72, $projWGS84, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($proj25833,$projWGS84,$pointSrc);
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projWGS84, $projL72, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projWGS84,$proj31468,$pointSrc);
 
-$pointSrc = new Point('-868208.53', '-1095793.57');
-$pointDest = $proj4->transform($proj5514,$projWGS84,$pointSrc);
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projL72, $proj25833, $pointSrc);
 
-$pointSrc = $pointDest;
-$pointDest = $proj4->transform($projWGS84,$proj5514,$pointSrc);
-}
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($proj25833, $projWGS84, $pointSrc);
+
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projWGS84, $proj31468, $pointSrc);
+
+        $pointSrc  = new Point('-868208.53', '-1095793.57');
+        $pointDest = $proj4->transform($proj5514, $projWGS84, $pointSrc);
+
+        $pointSrc  = $pointDest;
+        $pointDest = $proj4->transform($projWGS84, $proj5514, $pointSrc);
+    }
+
+
+    public function testProjFour()
+    {
+
+
+        $proj4           = new Proj4php();
+        $projL93         = new Proj('EPSG:2154', $proj4);
+        $projWGS84       = new Proj('EPSG:4326', $proj4);
+        $projLI          = new Proj('EPSG:27571', $proj4);
+        $projLSud        = new Proj('EPSG:27563', $proj4);
+        $projLSeventyTwo = new Proj('EPSG:31370', $proj4);
+
+
+        $pointSrc = new Point('652709.401', '6859290.946');
+        $this->assertEquals('652709.401 6859290.946', $pointSrc->toShortString());
+
+        $pointDest = $proj4->transform($projL93, $projWGS84, $pointSrc);
+        $this->assertEquals('2.3557811127971 48.831938054369', $pointDest->toShortString());
+
+        $pointDest = $proj4->transform($projWGS84, $projLSeventyTwo, $pointSrc);
+        $this->assertEquals('2354.4969810662 -51359.251012595', $pointDest->toShortString());
+
+        $pointDest = $proj4->transform($projLSeventyTwo, $projWGS84, $pointSrc);
+        $this->assertEquals('2.3557810993491 48.831938051733', $pointDest->toShortString());
+
+        $pointDest = $proj4->transform($projWGS84, $projLSud, $pointSrc);
+        $this->assertEquals('601419.93647681 726554.08663424', $pointDest->toShortString());
+
+        $pointDest = $proj4->transform($projLSud, $projWGS84, $pointSrc);
+        $this->assertEquals('2.3557810993491 48.831938051718', $pointDest->toShortString());
+
+        $pointDest = $proj4->transform($projWGS84, $projLI, $pointSrc);
+        $this->assertEquals('601415.06988072 1125718.0309796', $pointDest->toShortString());
+
+        $pointDest = $proj4->transform($projLI, $projL93, $pointSrc);
+        $this->assertEquals('652709.40001126 6859290.9458141', $pointDest->toShortString());
+    }
 }

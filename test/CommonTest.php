266cfe3e7569ed1e4be15e29efd91796f0374bef
robertbradshaw <rob@vegemitetoast.com>
2017-05-01 20:17:38 +1000

    fix to CommonTest

--- test/CommonTest.php
+++ test/CommonTest.php
@@ -1,13 +1,13 @@
 <?php
+
 include(__DIR__ . "/../vendor/autoload.php");
 
-use proj4php\Proj4php;
 use proj4php\Common;
-use proj4php\Proj;
 use proj4php\Point;
+use proj4php\Proj;
+use proj4php\Proj4php;
 
-class CommonTest
-    extends PHPUnit_Framework_TestCase
+class CommonTest extends PHPUnit_Framework_TestCase
 {
 
     public function testSign()

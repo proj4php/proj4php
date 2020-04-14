1095c129fed863f397ed31950ddb095776ab2571
Swen Zanon <1837968+swen100@users.noreply.github.com>
2020-04-14 07:55:20 +0200

    renames to 'EPSG:3857' in $default_defs

--- src/Proj4php.php
+++ src/Proj4php.php
@@ -69,7 +69,7 @@ class Proj4php
             'WGS84' => "+title=long/lat:WGS84 +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees",
             'EPSG:4326' => "+title=long/lat:WGS84 +proj=longlat +a=6378137.0 +b=6356752.31424518 +ellps=WGS84 +datum=WGS84 +units=degrees",
             'EPSG:4269' => "+title=long/lat:NAD83 +proj=longlat +a=6378137.0 +b=6356752.31414036 +ellps=GRS80 +datum=NAD83 +units=degrees",
-            'EPSG:3875' => "+title= Google Mercator +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs",
+            'EPSG:3857' => "+title= Google Mercator +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs",
         ];
 
         // Some defs are aliases.

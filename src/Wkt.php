7f5d2c1acaa264588a0329dffa4446d3be7a63b3
julien2512 <moquet.julien@gmail.com>
2021-10-30 15:01:27 +0200

    WKT parser spaces support

--- src/Wkt.php
+++ src/Wkt.php
@@ -8,6 +8,8 @@ class Wkt {
     private static $wktEllipsoids = array();
     private static $wktDatums = array();
 
+    private static $wktStrict = false;
+
     /**
      *
      * returns an associative array of projection arguments from a wkt string
@@ -351,6 +353,9 @@ class Wkt {
         $wktObject = $wktMatch[1];
         $wktContent = $wktMatch[2];
         $wktTemp = explode(",", $wktContent);
+
+        if (!self::$wktStrict)
+           $wktTemp = array_map('trim',$wktTemp);
 
         $wktName = (strtoupper($wktObject) == "TOWGS84") ? "TOWGS84" : trim(array_shift($wktTemp), '"');
 
